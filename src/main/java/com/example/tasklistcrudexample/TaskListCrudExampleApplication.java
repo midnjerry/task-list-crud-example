d1e427705815acc32cb282ca4eea24164cf1bfc4
NoobDad <mardaed10@gmail.com>
2022-10-06 16:03:31 -0500

    Initial project commit

---
+++ src/main/java/com/example/tasklistcrudexample/TaskListCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklistcrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasklistcrudexample/TaskListCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasklistcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
